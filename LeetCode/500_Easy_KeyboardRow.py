f77d8d5cd9e360ab9d7faf410dfea870ed44a905
GauthamCity23 <gauthamnagrajan@gmail.com>
2025-12-06 13:43:38 -0500

    Implement findWords method for keyboard row problem

---
+++ LeetCode/500_Easy_KeyboardRow.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        rows = {"q":1, "w":1, "e":1, "r":1, "t":1, "y":1, "u":1, "i":1, "o":1, "p":1,
+        "a":2, "s":2, "d":2, "f":2, "g":2, "h":2, "j":2, "k":2, "l":2,
+        "z":3, "x":3, "c":3, "v":3, "b":3, "n":3, "m":3, }
+        works = []
+        for word in words:
+            all_same = True
+            row_needed = rows[word[0].lower()]
+            for i in range(1, len(word)):
+                if rows[word[i].lower()] != row_needed:
+                    all_same = False
+                    break
+            if all_same:
+                works.append(word)
+        return works
